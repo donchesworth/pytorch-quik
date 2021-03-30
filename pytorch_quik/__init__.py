ced119030807f5f85faaed5c4500d12e0370769e
Don Chesworth <dcheswor@redhat.com>
2021-03-30 14:29:11 -0400

    don't require transformers/bert

--- pytorch_quik/__init__.py
+++ pytorch_quik/__init__.py
@@ -1,6 +1,11 @@
-import pytorch_quik.bert
 import pytorch_quik.ddp
 import pytorch_quik.transform
 import pytorch_quik.utils
 
-__all__ = ["bert", "ddp", "transform", "utils"]
+__all__ = ["ddp", "transform", "utils"]
+
+try:
+    import pytorch_quik.bert
+    __all__.append("bert")
+except ImportError:
+    print("skipping bert functions")
