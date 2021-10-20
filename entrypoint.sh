f6adb0ee85c569764bdd3d8c70178bd5a756124d
donchesworth <donald.chesworth@gmail.com>
2021-10-20 17:01:23 -0400

    small bug fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 cd /opt/pq
 # pytest
-pytest --mpl --cov=/opt/pq/pytorch_quik --cov-config=.coveragerc --cov-report=xml:coverage_cpu.xml --ignore=test_mlflow.py
+pytest --mpl --cov=/opt/pq/pytorch_quik --cov-config=.coveragerc --cov-report=xml:coverage_cpu.xml --ignore=pytorch_quik/tests/test_mlflow.py
 echo $(xmllint --xpath "string(//coverage/@line-rate)" coverage_cpu.xml)
 # curl https://codecov.io/bash
