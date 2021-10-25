d30c523f0cf065f21b7104bdaf8dac60e0281c3a
donchesworth <donald.chesworth@gmail.com>
2021-10-25 12:51:50 -0400

    removing tests to see problem

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 cd /opt/pq
 source ./.env
 # pytest
-pytest --mpl --cov=/opt/pq/pytorch_quik --cov-config=.coveragerc --cov-report=xml:coverage_cpu.xml
-# --ignore=pytorch_quik/tests/test_mlflow.py
+pytest --mpl --cov=/opt/pq/pytorch_quik --cov-config=.coveragerc --cov-report=xml:coverage_cpu.xml --ignore=pytorch_quik/tests/test_travel.py --ignore=pytorch_quik/tests/test_trek.py --ignore=pytorch_quik/tests/test_mlflow.py
 echo $(xmllint --xpath "string(//coverage/@line-rate)" coverage_cpu.xml)
 # curl https://codecov.io/bash
