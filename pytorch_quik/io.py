ba3a95a3abc818c4c391e239e0e447e43f94eadd
donchesworth <donald.chesworth@gmail.com>
2022-06-05 14:37:21 -0400

    remove api to serve-quik

--- pytorch_quik/io.py
+++ pytorch_quik/io.py
@@ -190,21 +190,6 @@ def serve_str(
         p = Path.cwd()
     p = p.joinpath(*jpath)
     t = p.parent.joinpath("tmp")
-    p.mkdir(parents = True, exist_ok = True)
-    t.mkdir(exist_ok = True)
+    p.mkdir(parents=True, exist_ok=True)
+    t.mkdir(exist_ok=True)
     return p, t
-
-
-def json_write(serve_path: Path, filename: str, data: str):
-    """Simple function to write json output to the appropriate
-    directory and filename.
-
-    Args:
-        serve_path (Path): Directory for model archive building
-        filename (str): Filename for the json output
-        data (str): String with json data to be written
-    """
-    file = Path.joinpath(serve_path, filename)
-    with open(file, "w") as outfile:
-        json.dump(data, outfile, indent=2)
-        outfile.write("\n")
